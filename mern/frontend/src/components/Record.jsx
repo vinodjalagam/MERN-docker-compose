67eb7861e5548f25687e35f38f33448e14c9b619
vinodjalagam <vinodjalagam477@gmail.com>
2024-09-04 11:05:41 +0530

    initial commit

--- mern/frontend/src/components/Record.jsx
+++ mern/frontend/src/components/Record.jsx
@@ -51,7 +51,7 @@ export default function Record() {
       let response;
       if (isNew) {
         // if we are adding a new record we will POST to /record.
-        response = await fetch("http://0.0.0.0:5050/record", {
+        response = await fetch("http://65.0.129.72:5050/record", {
           method: "POST",
           headers: {
             "Content-Type": "application/json",
@@ -60,7 +60,7 @@ export default function Record() {
         });
       } else {
         // if we are updating a record we will PATCH to /record/:id.
-        response = await fetch(`http://0.0.0.0:5050/record/${params.id}`, {
+        response = await fetch(`http://65.0.129.72:5050/record/${params.id}`, {
           method: "PATCH",
           headers: {
             "Content-Type": "application/json",
